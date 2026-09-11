7e542e1ce25dd0fc6077b299dfc11a5332e00c15
Cody-Moran <codymoran6m@gmail.com>
2026-09-11 14:44:19 -0600

    finished work

--- temp.py
+++ temp.py
@@ -30,3 +30,19 @@ total = (item_price*quantity+gst)
 print(f"Purchasing {quantity:.0f} of this item worth ${item_price} including the GST amount of $0.05 is ${total}")
 # %%
 
+# kilometers to miles converison
+# %%
+km = float(input ("Enter number of kilometers: "))
+miles = km*0.621371
+print(f"{km} converted into miles is: {miles:.2f}")
+# %%
+ 
+# fuel consumption calculation (cost of trip)
+# %%
+dist = float(input ("Enter the distance you want to travel (km): "))
+vehicle_consum = float(input ("Enter the fuel consumption of your vehicle (liters per 100km): "))
+price_per_litre = 1.58
+fuel_consum = (vehicle_consum*price_per_litre)
+trip_consum = (dist)*(fuel_consum)/100
+print(f"You are traveling {dist} kilometers, the amount of fuel needed for that distance is {vehicle_consum:.1f} liters. With the price of fuel being ${price_per_litre} per litre, this trip will cost ${trip_consum:.2f}")
+# %%
