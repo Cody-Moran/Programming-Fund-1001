b0b52d22848d6bfa073a40465a56948d72c01b55
Cody-Moran <codymoran6m@gmail.com>
2026-09-14 10:25:56 -0600

    Added a new file

--- temp.py
+++ temp.py
@@ -1,25 +1,25 @@
 # temp conversion program
 # the formula is °F = (°C × 9/5) + 32
-
+# %%
 user_input = float(input("Input celsius temp here: "))
 output = user_input*9/5+32
 print(f"{user_input} celsius converted to farenheit is: {output}")
-
+# %%
 # height and width of rectangle calculation
 
-width = float(input("Enter the width of a rectangle: "))
-height = float(input("Enter the height of a rectangle: "))
+width = float(input("Enter the width of a rectangle (cm): "))
+height = float(input("Enter the height of a rectangle (cm): "))
 area = width * height
 perimeter = 2*(area)
 
-print(f"Area: {area:.3f}")  
+print(f"The area of the rectangle is: {area:.3f} cm")  
 
 # printing a silly sentence
 # %%
 name = input("What is your name?: ")
 adj = "hard"
 verb = "jumped"
-print(f"{name} {verb} really {adj}")
+print(f"{name} {verb} really {adj}.")
 # %%
 # price and quantity of item GST
 # %%
@@ -28,15 +28,13 @@ quantity = float(input("Quantity: "))
 gst = ((quantity*item_price)*0.05)
 total = (item_price*quantity+gst)
 print(f"Purchasing {quantity:.0f} of this item worth ${item_price} including the GST amount of $0.05 is ${total}")
-# %%
 
 # kilometers to miles converison
 # %%
 km = float(input ("Enter number of kilometers: "))
 miles = km*0.621371
 print(f"{km} converted into miles is: {miles:.2f}")
-# %%
- 
+
 # fuel consumption calculation (cost of trip)
 # %%
 dist = float(input ("Enter the distance you want to travel (km): "))
